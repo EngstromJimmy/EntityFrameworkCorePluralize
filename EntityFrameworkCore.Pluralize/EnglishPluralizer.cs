682d0c6b5ea645635e9cf531f349be1ac17045f2
Jimmy Engström <jimmy@apeoholic.se>
2018-04-06 17:28:54 +0200

    Initial commit for project files

---
+++ EntityFrameworkCore.Pluralize/EnglishPluralizer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data.Entity.Design.PluralizationServices;
+
+public class EnglishPluralizer : IPluralizer
+{
+    EnglishPluralizationService pluralizationService = new EnglishPluralizationService();
+    public string Pluralize(string name)
+    {
+
+        return pluralizationService.Pluralize(name);
+    }
+
+    public string Singularize(string name)
+    {
+        return pluralizationService.Singularize(name);
+    }
+}
